94de8426567e541b258f0bca35b138ab8b94070e
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 20:48:46 +0530

    Update AreaofRhombus.java

--- AreaofRhombus.java
+++ AreaofRhombus.java
@@ -11,6 +11,8 @@ public class AreaofRhombus{
     }
 }
 
+OUTPUT:
+
 C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=7913:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\Java 1\out\production\Java 1" AreaofRhombus
 Enter the first diagonal length(d1):
 12
