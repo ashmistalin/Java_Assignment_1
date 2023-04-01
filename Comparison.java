598fc987d1c0b2954a2ef4fc3d38a463685181e5
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 20:49:26 +0530

    Update Comparison.java

--- Comparison.java
+++ Comparison.java
@@ -19,3 +19,12 @@ public class Comparison{
         }
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=8098:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\Java 1\out\production\Java 1" Comparison
+Enter the numbers:
+10 45
+45 is greater than 10
+
+Process finished with exit code 0
