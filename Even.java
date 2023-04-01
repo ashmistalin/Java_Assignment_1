e4fb738204332298446cf2ae501ca3407fa28e7d
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 20:50:50 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -11,3 +11,19 @@ public class Even{
         }
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=8209:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\Java 1\out\production\Java 1" Even
+The even numbers from 1 to 20 are listed below:
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+Process finished with exit code 0
