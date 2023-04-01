f80a4b927e0d5cb231bde0de9e8f0c09df8aae54
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 20:51:49 +0530

    Update Tables.java

--- Tables.java
+++ Tables.java
@@ -13,3 +13,21 @@ public class Tables{
         }
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=8261:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\Java 1\out\production\Java 1" Tables
+Enter the number for which the table has to generated: 5
+Enter the limit for the table: 10
+5x1=5
+5x2=10
+5x3=15
+5x4=20
+5x5=25
+5x6=30
+5x7=35
+5x8=40
+5x9=45
+5x10=50
+
+Process finished with exit code 0
