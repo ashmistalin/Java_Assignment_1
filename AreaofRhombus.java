046543285f3a5c3810c6efeafc84266b70c02ef8
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 20:47:19 +0530

    Update AreaofRhombus.java

--- AreaofRhombus.java
+++ AreaofRhombus.java
@@ -10,3 +10,12 @@ public class AreaofRhombus{
         System.out.println("The area of the rhombus in sq. units: "+area);
     }
 }
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=7913:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\Java 1\out\production\Java 1" AreaofRhombus
+Enter the first diagonal length(d1):
+12
+Enter the second diagonal length(d2):
+23.09
+The area of the rhombus in sq. units: 138.54001
+
+Process finished with exit code 0
