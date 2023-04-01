952424a90dbf389cee8eccd5fb4c75853f30437a
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 20:52:26 +0530

    Update StringToInteger.java

--- StringToInteger.java
+++ StringToInteger.java
@@ -8,3 +8,12 @@ public class StringToInteger{
         System.out.println("The integer value of the string is: "+num);
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=8334:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\Java 1\out\production\Java 1" StringToInteger
+Enter the string:
+1234
+The integer value of the string is: 1234
+
+Process finished with exit code 0
